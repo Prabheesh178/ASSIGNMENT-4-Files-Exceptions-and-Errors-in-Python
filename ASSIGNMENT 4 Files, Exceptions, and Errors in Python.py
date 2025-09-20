908e10dfaf050f4855fac098184402ce11025a7f
Prabheesh178 <85186879+Prabheesh178@users.noreply.github.com>
2025-09-20 14:59:55 +0530

    Add files via upload

---
+++ ASSIGNMENT 4 Files, Exceptions, and Errors in Python.py
@@ -0,0 +1,20 @@
+try:
+    with open('sample.txt','r') as file1:
+        reading_file=file1.read()
+    print(reading_file)
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' does not exist.")
+
+
+
+
+#task 2 
+user_input= input ('entersome data to write in the file : ')
+with open ("output.txt",'w') as file2:
+    writing_file=file2.write(user_input+'\n')
+    print('the number of character writen=',writing_file)
+
+append_data=input ("enter some additional data to add int the file: ")
+with open ('output.txt','a') as file2:
+    appending_file=file2.write(append_data+'\n')
+    print('the number of character writen=',appending_file)
